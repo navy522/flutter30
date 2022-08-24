b16c9c395ba8a5038e134a3be6fd5db69cbdd8aa
navy522 <navneetsingh813@gmail.com>
2022-08-24 16:14:05 +0530

    day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/home_page.dart';
+import 'package:flutter_catalog/pages/home_page.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: HomePage(),
+    themeMode: ThemeMode.light,
+    theme: ThemeData(primarySwatch: Colors.orange),
+    darkTheme: ThemeData(primarySwatch: Colors.red),
+    initialRoute: "/login",
+    routes: {
+      "/": (context) => HomePage(),
+      "/login": (context) => LoginPage(),
+    },
   ));
 }
-
-
